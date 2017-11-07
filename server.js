911f148d0e60184be1dceb364609ac0ab722a35f
Jay Lara <jay.lara@outlook.com>
2017-11-06 18:20:12 -0600

    cleaned up code. added comments. added more paths

--- server.js
+++ server.js
@@ -1,62 +1,83 @@
 // dependencies
 const yelp = require('yelp-fusion');
-const axios = require('axios');
-const express = require('express');
+const app = require('express')();
 const bodyParser = require('body-parser');
 
 require('dotenv').config();
 // app config
-const app = express();
+//const app = express();
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
-//route variables
+//executes handling of a promise. responds with successful data output
+// or error message in json
+var handleClientAction = (res, promise) => {
+	promise.then(response => {
+			res.json(response.jsonBody);
+	}).catch(e => {
+			res.json(e);
+	});
+}//end of handleClientAction()
+
 // define a root route:
 app.get('/', (req, res) => {
-	res.send('user /yelp');
+	res.send('Try /search, /');
 });
 
+//get access token using a promise
 const yelpClientPromise = yelp.accessToken(process.env.CLIENT_ID, process.env.CLIENT_SECRET)
-.then(response => {
-	console.log(response.jsonBody.access_token);
-	return yelp.client(response.jsonBody.access_token);
-}).catch(e => {
-	console.log(e);
-});
+	.then(res => {
+		return yelp.client(res.jsonBody.access_token);
+	}).catch(e => {
+		console.log(e);
+		res.status(500).send('Could not get Yelp Access Token');
+	});
 
-app.get('/yelp',(req, res) => {
+//Simple search. handles query strings.
+//simplest query requires value for [location] or [latitude]+[longitude] values.
+app.get('/search',(req, res) => {
 	yelpClientPromise.then(client => {
-		//do stuff with `yelp`
-		client.business('gary-danko-san-francisco').then(response => {
-		  		res.json(response.jsonBody);
-				}).catch(e => {
-		  		res.json(e);
-				});
+		handleClientAction(res, client.search(req.query));
 	}).catch(err => {
 		res.status(500).send('Could not get Yelp client');
 	});
+});//end of GET/search route
+
+//Phone search. handles query strings.
+//simplest query needs value for [phone] value (ex: +14157492060).
+app.get('/phone',(req, res) => {
+	yelpClientPromise.then(client => {
+		handleClientAction(res, client.phoneSearch(req.query));
+	}).catch(err => {
+		res.status(500).send('Could not get Yelp client');
+	});
+});//end of GET/phone route
 
 
-	// let url = 'https://api.yelp.com/v2';
-  //     axios.get(url,{
-  //       //params
-  //       'headers': {
-  //         'Authorization': 'Bearer ' + this.access.access_token,
-  //         'Access-Control-Allow-Origin' : '*',
-  //         'Access-Control-Allow-Headers' : ['GET']
-  //       }
-  //     }).then((response)=>{
-  //       console.log(response);
-  //     }).catch(e=>e);
-	// axios.get('http://api.icndb.com/jokes/random')
-	// .then((response) =>{
-	// 	console.log(response);
-	// 	res.json(response.data);
-	// })
-	// .catch((error) => {
-	// 	console.log(error);
-	// })
-});
+//TODO: needs some work. https://www.yelp.com/developers/documentation/v3/business_reviews
+//reviews search. handles query strings.
+//simplest query needs value for [term] value (ex: ).
+app.get('/reviews',(req, res) => {
+	yelpClientPromise.then(client => {
+		handleClientAction(res, client.reviews(req.query.term));
+	}).catch(err => {
+		res.status(500).send('Could not get Yelp client');
+	});
+});//end of GET/reviews route
+
+
+//autocomplete search. handles query strings.
+//simplest query requires value for [text] value (ex: pizza).
+app.get('/autocomplete',(req, res) => {
+	yelpClientPromise.then(client => {
+		handleClientAction(res, client.autocomplete(req.query.text));
+	}).catch(err => {
+		res.status(500).send('Could not get Yelp client');
+	});
+});//end of GET/reviews route
+
+
+
 
 // start app
 app.listen(port, function(err) {
